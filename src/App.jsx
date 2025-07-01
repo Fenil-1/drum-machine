57a87b1013740f6aac049177fe79e10b3c6cc373
Fenil-1 <fenilp2046@gmail.com>
2025-07-01 13:31:52 +0530

    Basic templete added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Button, Typography } from '@mui/material'
+import Drum from './components/Drum'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Drum />
+    </>
+  )
+}
+
+export default App
